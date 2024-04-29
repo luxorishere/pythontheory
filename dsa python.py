d5d3c958ee0b55f3dd806f373ac94ad1a6401dd6
Parth <justforbusinessforcoding@gmail.com>
2024-04-29 23:38:38 +0530

    adding the file for notes

--- dsa python.py
+++ dsa python.py
@@ -1,30 +1,5 @@
-def leetcode(ilist, account):
-    sum = 0
-    i = len(ilist) - 1
-    count = 0
-    if account < ilist[i]:
-        return 0
-    while sum != account or i != 0:
-        temp = sum
-        temp += ilist[i]
-        if temp <=  account: 
-            
-            sum += ilist[i]
-            count += 1
-            
-        else:
-            i -= 1
-    while account != sum:
-        ilist.pop()
-        leetcode(ilist, account)
-        
-        
-    if count != 0:
-        return count
-    else:
-        return -1
+def print_birthday():
+    for i in range(10):
+        print("Happy Birthday")
 
-print(leetcode([2,3,5], 11))
-        
-            
-        
+print_birthday()
